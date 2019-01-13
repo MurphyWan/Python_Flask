37b3c8118ffa1133f208b29cf0ebfd4c935fb600
murphywan <murphywan@gmail.com>
2019-01-14 00:43:13 +0800

    7/5 26'41" user/change old_password to new_password  (done)

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -91,10 +91,10 @@ def edit():
 
     resp = { 'code':200, 'msg':'操作成功～', 'data':{} }
     req = request.values
-    nickname = req['nickname'] if 'nickname' in req else ''
+    old_password = req['old_password'] if 'old_password' in req else ''
     email = req['email'] if 'email' in req else ''
 
-    if nickname is None or len( nickname ) < 1:
+    if old_password is None or len( old_password ) < 1:
         resp['code']  = -1
         resp['msg'] = "请输入符合规范的姓名～"
         return jsonify( resp )
@@ -107,7 +107,7 @@ def edit():
     #当前用户已经查出来了，所以不用再查了。读出来就好。;引入g变量？？？
     user_info = g.current_user
     #将user的nickname和email进行更新
-    user_info.nickname = nickname
+    user_info.old_password = old_password
     user_info.email = email
 
     #然后进行统一的数据库提交;import db
@@ -121,9 +121,49 @@ def edit():
 
 
 
-@route_user.route("/reset-pwd")
+# 依然是用GET和POST两种方式展示提交的
+@route_user.route("/reset-pwd", methods= ["GET","POST"])
 def resetPwd():
-    return ops_render("user/reset_pwd.html")
+    # 如果请求是GET，我们就进行展示就可以了
+    if request.method == "GET":
+        return ops_render("user/reset_pwd.html")
+
+    resp = { 'code':200, 'msg':'操作成功', 'data':{} }
+    req = request.values
+
+
+    old_password = req['old_password'] if  'old_password' in req else ''
+    new_password = req['new_password'] if  'new_password' in req else ''
+
+    # 同样进行参数有效性判断,对原密码和新密码
+    if old_password is None or len(old_password) <6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的原始密码"
+        return jsonify(resp)
+    #新密码要进行2次判断，空或者长度不足
+    if new_password is None or len(new_password) <6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的新密码"
+        return jsonify(resp)
+    #新旧密码一样
+    if old_password == new_password:
+        resp['code'] = -1
+        resp['msg'] = "请重新输入符合规范的新密码，新旧密码不能相同哦！"
+        return jsonify(resp)
+    #以上参数已经校验完了。
+
+    # 下一步,进行密码的更改。同样的从g.current_user取出当前的uesr对象
+    user_info = g.current_user
+    #注意，修改密码可不是明文哦！！！UserService.py
+    user_info.login_pwd = UserService.genePwd( new_password , user_info.login_salt)
+
+    db.session.add( user_info )
+    db.session.commit() #统一提交数据库
+    return jsonify( resp ) #统一提交返回
+
+
+
+
 
 # 退出 ; 这个功能比较简单，只需要将cookie清空完了就可以退出了。具体就是清理cookie，并且跳到登录页面。
 @route_user.route( "/logout" )
--- web/static/js/user/reset_pwd.js
+++ web/static/js/user/reset_pwd.js
@@ -1,4 +1,8 @@
 ;
+//这个js文件是老师已经帮我们写好的，直接用就可以了。其目的是将前台提交的用户昵称和手机号和邮箱传递到后台更新数据库
+//定义变量user_edit_ops，里面有2个方法：init和eventBind，和所有其他js一样。
+//因为这个文件类似于edit.js，所以复值过来的
+//在reset_pwd.html中式哟个id方式save，所以我们这个方法就是用id方式
 var mod_pwd_ops = {
     init:function(){
         this.eventBind();
@@ -10,30 +14,32 @@ var mod_pwd_ops = {
                 common_ops.alert("正在处理!!请不要重复提交~~");
                 return;
             }
-
+//上面部分是以上的；然后获取我们表单的值
             var old_password = $("#old_password").val();
             var new_password = $("#new_password").val();
-
-
+            //var nickname = nickname_target.val();
+//获取完毕之后就是我们参数有效性的校验部分
+            //如果没有原始密码
             if( !old_password ){
                 common_ops.alert( "请输入原密码~~" );
                 return false;
             }
-
-            if( !new_password || new_password.length < 6 ){
+            //如果没有新密码，且新密码长度小于6
+            if( !new_password || new_password < 6 ){
                 common_ops.alert( "请输入不少于6位的新密码~~" );
                 return false;
             }
 
             btn_target.addClass("disabled");
 
+            //传递到我们后端去
             var data = {
                 old_password: old_password,
                 new_password: new_password
             };
 
             $.ajax({
-                url:common_ops.buildUrl( "/user/reset-pwd" ),
+                url:common_ops.buildUrl( "/user/reset-pwd" ),//方法名是reset-pwd
                 type:'POST',
                 data:data,
                 dataType:'json',
@@ -54,6 +60,8 @@ var mod_pwd_ops = {
     }
 };
 
+//当我们jquery加载完成的时候，我们执行我们的init方法就可以了。
 $(document).ready( function(){
     mod_pwd_ops.init();
 } );
+//写完这个js文件，回到reset_pwd.html文件，将本js引入进去
