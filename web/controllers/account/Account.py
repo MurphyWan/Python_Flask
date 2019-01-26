a8219521b29b92303b032ea541ad5691c29d5f73
murphywan <murphywan@gmail.com>
2019-01-26 20:17:16 +0800

    7/6 ~3'35" show account list (done)

--- web/controllers/account/Account.py
+++ web/controllers/account/Account.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
+# 2019/01/26 01 账号管理
 from flask import Blueprint
 from common.libs.Helper import ops_render
+from common.models.User import User
+
 
 route_account = Blueprint( 'account_page',__name__ )
 
 @route_account.route( "/index" )
 def index():
-    return ops_render( "account/index.html" )
+    # 账号列表展示，从数据库中读取出来，然后展示即可
+    # 首先从common.models.User中导入User
+    # 全量查询，分页功能一会儿再说;用uid这个字段进行倒序排列;.all()取出所有的数据，得到一个list
+    # 将这个list传递到展示页面就可以展示了;定义一个resp_data
+    resp_data ={} # 定义一个json变量
+    list = User.query.order_by( User.uid.desc() ).all()
+    resp_data['list'] = list
+
+    return ops_render( "account/index.html", resp_data ) # 将list的值通过jason变量传进来；打开模板页面template/account/index.html
 
 @route_account.route( "/info" )
 def info():
